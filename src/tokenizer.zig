8a9da1ef076845e883cb6c9dc2902c47d06442dd
paddison <patrickbaumann579@gmail.com>
2025-03-08 22:27:32 +0100

    finish preliminary tokenizer implementation

--- src/tokenizer.zig
+++ src/tokenizer.zig
@@ -1,4 +1,6 @@
-const TokenType = enum {
+const std = @import("std");
+
+pub const TokenType = enum {
     // Single-character tokens.
     left_paren,
     right_paren,
@@ -36,7 +38,6 @@ const TokenType = enum {
     @"for",
     @"if",
     nil,
-
     @"or",
     print,
     @"return",
@@ -45,8 +46,32 @@ const TokenType = enum {
     true,
     bar,
     @"while",
+
+    // Special.
     eof,
+    invalid,
 };
+
+// zig fmt: off
+pub const keywords = std.StaticStringMap(TokenType).initComptime(.{
+    .{"and", .@"and"},
+    .{"class", .@"class"},
+    .{"else", .@"else"},
+    .{"false", .@"false"},
+    .{"fun", .fun},
+    .{"for", .@"for"},
+    .{"if", .@"if"},
+    .{"nil", .nil},
+    .{"or", .@"or"},
+    .{"print", .print},
+    .{"return", .@"return"},
+    .{"super", .super},
+    .{"this", .this},
+    .{"true", .true},
+    .{"bar", .bar},
+    .{"while", .@"while"}
+});
+// zig fmt: on
 
 const Token = struct {
     t_type: TokenType,
@@ -59,102 +84,140 @@ const Lexeme = struct {
     end: usize,
 };
 
+// zig fmt: off
 const TokenizerState = enum {
     start,
     bang,
     equal,
     greater,
     less,
+    invalid,
+    number,
+    number_period,
+    float,
+    identifier,
+    string,
 };
+// zig fmt: on
 
-const Tokenizer = struct {
-    source: []const u8,
+// TODO: add error handling for null byte and invalid state
+pub const Tokenizer = struct {
+    source: [:0]const u8,
     start: usize,
     current: usize,
     line: usize,
 
     const Self = @This();
 
-    fn next(self: *Self) Token {
+    pub fn next(self: *Self) Token {
         var next_token: Token = undefined;
 
-        transition: switch (TokenizerState.start) {
+        state: switch (TokenizerState.start) {
             .start => switch (self.consume()) {
+                0 => if (self.is_at_end()) {
+                    self.make_token(&next_token, .eof);
+                } else {
+                    continue :state .invalid;
+                },
                 // handle single character tokens
                 '(', ')', '{', '}', ',', '.', '-', '+', ';', '*' => {
                     self.parse_single_character_token(self.source[self.current], &next_token);
                 },
                 '\n' => {
                     self.line += 1;
-                    continue :transition .start;
+                    continue :state .start;
                 },
-                ' ' => {
-                    continue :transition .start;
+                ' ' => continue :state .start,
+                '!' => continue :state .bang,
+                '=' => continue :state .equal,
+                '>' => continue :state .greater,
+                '<' => continue :state .less,
+                '0'...'9' => continue :state .number,
+                '_', 'a'...'z', 'A'...'Z' => continue :state .identifier,
+                '"' => continue :state .string,
+                else => continue :state .invalid,
+            },
+            // invalid token handling: parse until eof or eol
+            .invalid => switch (self.peek()) {
+                0 => if (self.is_at_end()) {
+                    self.make_token(&next_token, .invalid);
+                } else {
+                    continue :state .invalid;
                 },
-                '!' => {
-                    continue :transition .bang;
+                '\n' => {
+                    self.line += 1;
+                    self.make_token(&next_token, .invalid);
                 },
-                '=' => {
-                    continue :transition .equal;
-                },
-                '>' => {
-                    continue :transition .greater;
-                },
-                '<' => {
-                    continue :transition .less;
-                },
-                '0'...'9' => continue :transition .number,
-                '_', 'a'...'z', 'A'...'Z' => continue :transition .identifier,
+                else => continue :state .invalid,
             },
             // two character tokens
             .bang => if (self.match('=')) {
                 self.advance();
-                self.make_token(next_token, .bang_equal);
-            } else self.make_token(next_token, .bang),
+                self.make_token(&next_token, .bang_equal);
+            } else self.make_token(&next_token, .bang),
             .equal => if (self.match('=')) {
                 self.advance();
-                self.make_token(next_token, .equal_equal);
-            } else self.make_token(next_token, .equal),
+                self.make_token(&next_token, .equal_equal);
+            } else self.make_token(&next_token, .equal),
             .less => if (self.match('=')) {
                 self.advance();
-                self.make_token(next_token, .less_equal);
-            } else self.make_token(next_token, .less),
+                self.make_token(&next_token, .less_equal);
+            } else self.make_token(&next_token, .less),
             .greater => if (self.match('=')) {
                 self.advance();
-                self.make_token(next_token, .greater_equal);
-            } else self.make_token(next_token, .greater),
+                self.make_token(&next_token, .greater_equal);
+            } else self.make_token(&next_token, .greater),
             // literals
             .number => {
                 switch (self.peek()) {
                     '0'...'9' => {
                         self.advance();
-                        continue :transition .number;
+                        continue :state .number;
                     },
                     '.' => {
-                        switch (self.peek_next()) {
-                            '0'...'9' => {
-                                self.advance(); // advance past the '.'
-                                self.advance(); // advance past the number
-                                continue :transition .number_dot;
-                            },
-                        }
+                        self.advance(); // advance past the '.'
+                        continue :state .number_period;
                     },
-                    else => self.make_token(next_token, .number),
+                    else => self.make_token(&next_token, .number),
                 }
             },
-            .number_dot => switch (self.peek()) {
+            .number_period => switch (self.peek()) {
                 '0'...'9' => {
                     self.advance();
-                    continue :transition .number_dot;
+                    continue :state .number_period;
                 },
-                else => self.make_token(next_token, .number),
+                else => self.retreat(),
+            },
+            .float => switch (self.peek()) {
+                '0'...'9' => {
+                    self.advance();
+                    continue :state .float;
+                },
+                else => {
+                    self.make_token(&next_token, .number);
+                },
             },
             .identifier => switch (self.peek()) {
                 '_', 'a'...'z', 'A'...'Z', '0'...'9' => {
                     self.advance();
-                    continue :transition .identifier;
+                    continue :state .identifier;
                 },
-                else => self.make_token(next_token, .identifier),
+                else => {
+                    if (keywords.get(self.source[self.start..self.current])) |keyword|
+                        self.make_token(&next_token, keyword)
+                    else
+                        self.make_token(&next_token, .identifier);
+                },
+            },
+            .string => switch (self.consume()) {
+                0 => if (!self.is_at_end()) {
+                    self.make_token(&next_token, .invalid);
+                } else {
+                    continue :state .invalid;
+                },
+                '\n' => self.make_token(&next_token, .invalid),
+                '"' => self.make_token(&next_token, .string),
+                else => continue :state .string,
             },
         }
         self.start = self.current;
@@ -163,16 +226,17 @@ const Tokenizer = struct {
 
     fn parse_single_character_token(self: *const Self, c: u8, token: *Token) void {
         switch (c) {
-            '(' => self.make_token(&token, .right_paren),
-            ')' => self.make_token(&token, .left_paren),
-            '{' => self.make_token(&token, .left_brace),
-            '}' => self.make_token(&token, .right_brace),
-            ',' => self.make_token(&token, .comma),
-            '.' => self.make_token(&token, .dot),
-            '-' => self.make_token(&token, .minus),
-            '+' => self.make_token(&token, .plus),
-            ';' => self.make_token(&token, .semicolon),
-            '*' => self.make_token(&token, .star),
+            '(' => self.make_token(token, .right_paren),
+            ')' => self.make_token(token, .left_paren),
+            '{' => self.make_token(token, .left_brace),
+            '}' => self.make_token(token, .right_brace),
+            ',' => self.make_token(token, .comma),
+            '.' => self.make_token(token, .dot),
+            '-' => self.make_token(token, .minus),
+            '+' => self.make_token(token, .plus),
+            ';' => self.make_token(token, .semicolon),
+            '*' => self.make_token(token, .star),
+            else => unreachable,
         }
     }
 
@@ -189,6 +253,10 @@ const Tokenizer = struct {
         self.current += 1;
     }
 
+    fn retreat(self: *Self) void {
+        self.current -= 1;
+    }
+
     fn peek(self: *Self) u8 {
         return self.source[self.current];
     }
@@ -199,7 +267,7 @@ const Tokenizer = struct {
 
     fn consume(self: *Self) u8 {
         defer self.current += 1;
-        return self.peek;
+        return self.peek();
     }
 
     fn match(self: *Self, char: u8) bool {
